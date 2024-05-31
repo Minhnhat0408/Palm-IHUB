6f4261ea0b5ffc26ea3abae8d42f7a1433cd8879
MinhMatMong <kurominhnhat@gmail.com>
2024-05-31 22:12:47 +0700

    fix deploy

--- src/components/animations/slider-multiple.tsx
+++ src/components/animations/slider-multiple.tsx
@@ -126,19 +126,19 @@ export default function SliderMultiple({
     );
 }
 
-function Arrow(props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) {
-    const disabeld = props.disabled ? ' fill-muted-foreground' : '';
-    return (
-        <svg
-            onClick={props.onClick}
-            className={`absolute  top-1/2  h-10 w-10 -translate-y-[50%] cursor-pointer fill-black opacity-0 duration-500 group-hover:opacity-100 ${
-                props.left ? ' -left-10' : ' -right-10'
-            } ${disabeld}`}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-        >
-            {props.left && <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />}
-            {!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
-        </svg>
-    );
-}
+// function Arrow(props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) {
+//     const disabeld = props.disabled ? ' fill-muted-foreground' : '';
+//     return (
+//         <svg
+//             onClick={props.onClick}
+//             className={`absolute  top-1/2  h-10 w-10 -translate-y-[50%] cursor-pointer fill-black opacity-0 duration-500 group-hover:opacity-100 ${
+//                 props.left ? ' -left-10' : ' -right-10'
+//             } ${disabeld}`}
+//             xmlns="http://www.w3.org/2000/svg"
+//             viewBox="0 0 24 24"
+//         >
+//             {props.left && <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />}
+//             {!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
+//         </svg>
+//     );
+// }
