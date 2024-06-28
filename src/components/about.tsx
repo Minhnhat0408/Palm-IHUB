704b702b0a6fda67f6721e5353c95bafa0ad1990
MinhMatMong <kurominhnhat@gmail.com>
2024-06-28 17:24:38 +0700

    update ver 2.0

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,42 +1,83 @@
+import { useRef } from 'react';
 import Reveal from './animations/reveal';
 
+import hand from '/scan.svg';
+
 export default function About() {
+    const isMobile = useRef(() => {
+        if (window.innerWidth < 1024) {
+            return {
+                hiddenY: 200,
+                initial: 'visible',
+                delay: 1.2,
+            };
+        }
+        return {
+            hiddenY: 200,
+            delay: 1,
+        };
+    });
+
+    const textRevealStyle = useRef(() => {
+        if (window.innerWidth < 1024) {
+            return {
+                scale: 0.5,
+                delay: 1.2,
+            };
+        }
+        return {
+            hiddenX: -100,
+        };
+    });
+
     return (
         <section
-            className="relative h-screen overflow-hidden px-10 py-40 lg:overflow-visible  lg:py-20 xl:px-20"
+            className="relative h-screen overflow-hidden bg-[#121212] px-6 py-40 sm:px-10  lg:py-20 xl:px-20"
             id="tech"
         >
-            <div className="absolute -top-20 left-0 right-0 opacity-80 hidden lg:block">
+            {/* <div className="absolute -top-20 left-0 right-0 hidden opacity-80 lg:block">
                 <img src="/Gradient 2.webp" alt="palm" />
-            </div>
-            <div className=" grid lg:grid-cols-2  ">
-                <Reveal
-                    hiddenX={-100}
-                    className="flex flex-col justify-center space-y-8 px-4 pb-10 text-justify text-lg  sm:px-10 sm:text-2xl md:text-left xl:px-20"
-                >
-                    <h2 className="text-center  text-4xl font-bold sm:text-5xl  lg:text-6xl 2xl:text-7xl ">
-                        Bạn có biết
-                    </h2>
-                    <p className=" font-semibold">
-                        Bên dưới lớp da là một mạng lưới mạch máu phức tạp, cấu trúc không đổi và duy nhất đối với từng
-                        người.
-                    </p>
-                    <p className=" font-semibold">
-                        Chúng tôi đã phát triển công nghệ{' '}
-                        <span className="text-gradient text-xl font-bold sm:text-3xl ">Palm ID</span> sử dụng tia hồng
-                        ngoại để chụp lại cấu trúc này, và sử dụng nó như một chiếc chìa khóa nhằm giúp bạn xác nhận
-                        danh tính trong các hoạt động của cuộc sống.
-                    </p>
-                </Reveal>
-                <div className="absolute left-0 top-20 flex  w-full   justify-center   opacity-60  sm:items-center lg:relative">
-                    <Reveal scale={0.7}>
-                        <img
-                            src="/hand-orange.webp"
-                            alt="about"
-                            className="-left-10 h-[75vh] object-cover opacity-50  sm:opacity-100  "
-                        />
+            </div> */}
+            <div className=" grid h-full lg:grid-cols-2  ">
+                {textRevealStyle && (
+                    <Reveal
+                        {...textRevealStyle.current()}
+                        // initial='visible'
+                        className="z-[10] flex flex-1 flex-col justify-center space-y-8 px-4 pb-10  text-justify text-lg text-white  sm:px-10 sm:text-2xl md:text-left xl:px-20"
+                    >
+                        <h2 className="text-gradient pb-3 text-center text-4xl font-bold sm:text-5xl  lg:text-6xl 2xl:text-7xl ">
+                            Bạn có biết
+                        </h2>
+                        <p className=" font-semibold">
+                            Bên dưới lớp da là một mạng lưới mạch máu phức tạp, cấu trúc không đổi và duy nhất đối với
+                            từng người.
+                        </p>
+                        <p className=" font-semibold">
+                            Chúng tôi đã phát triển công nghệ{' '}
+                            <span className="text-gradient text-xl font-bold sm:text-3xl ">Palm ID</span> sử dụng tia
+                            hồng ngoại để chụp lại cấu trúc này, và sử dụng nó như một chiếc chìa khóa nhằm giúp bạn xác
+                            nhận danh tính trong các hoạt động của cuộc sống.
+                        </p>
                     </Reveal>
+                )}
+                <div className="absolute left-0  top-1/2 flex w-full  -translate-y-1/2  justify-center opacity-60  sm:items-center   lg:relative  lg:top-0 lg:translate-y-0">
+                    <div className="relative flex w-[40%] items-center sm:w-[30%] lg:w-[38%]  xl:w-[35%] 2xl:w-[30%] ">
+                        <div className="aspect-square w-full opacity-65 lg:opacity-100">
+                            <div className="box  aspect-square w-full bg-[#121212]"></div>
+                        </div>
+                        {isMobile && (
+                            <Reveal
+                                {...isMobile.current()}
+                                className="absolute -left-[60%] w-[250%] lg:-left-[80%]  lg:w-[280%] "
+                            >
+                                <img src={hand} alt="about" className="  object-cover " />
+                            </Reveal>
+                        )}
+                    </div>
                 </div>
+                {/* <div className="flex w-full items-center justify-center ">
+                    <div className="box aspect-square w-1/2 bg-[#121212]"></div>
+                </div> */}
             </div>
         </section>
     );
--- src/components/animations/reveal.tsx
+++ src/components/animations/reveal.tsx
@@ -1,109 +1,109 @@
-import { motion, useInView, useAnimation } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 type Animate = {
-  opacity: number;
-  y?: number;
-  x?: number;
-  scale?: number;
+    opacity: number;
+    y?: number;
+    x?: number;
+    scale?: number;
+};
+export type RevealProps = {
+    hiddenY?: number;
+    hiddenX?: number;
+    duration?: number;
+    scale?: number;
+    delay?: number;
+    initial?: string;
+    preanimation?: boolean;
+    className?: string;
 };
 function Reveal({
-  children,
-  hiddenY,
-  hiddenX,
-  duration = 0.8,
-  scale,
-  delay,
-  initial = "hidden",
-  preanimation = false,
-  className,
-}: {
-  children: React.ReactNode;
-  hiddenY?: number;
-  hiddenX?: number;
-  duration?: number;
-  scale?: number;
-  delay?: number;
-  initial?: string;
-  preanimation?: boolean;
-  className?: string;
-}) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-  const mainControls = useAnimation();
-  const slideControls = useAnimation();
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start("visible");
-      slideControls.start("visible");
-    }
-  }, [isInView, mainControls, slideControls]);
+    children,
+    hiddenY,
+    hiddenX,
+    duration = 0.8,
+    scale,
+    delay,
+    initial = 'hidden',
+    preanimation = false,
+    className,
+}: RevealProps & { children: React.ReactNode }) {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+    const mainControls = useAnimation();
+    const slideControls = useAnimation();
+    useEffect(() => {
+        if (isInView) {
+            mainControls.start(initial === 'hidden' ? 'visible' : 'hidden');
+            slideControls.start(initial === 'hidden' ? 'visible' : 'hidden');
+        }
+    }, [isInView, mainControls, slideControls]);
 
-  function getVariant() {
-    const variants = {
-      hidden: { opacity: 0 },
-      visible: { opacity: 1 },
-    };
+    function getVariant() {
+        const variants = {
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+        };
 
-    if (hiddenY) {
-      variants.hidden = { ...variants.hidden, y: hiddenY } as Animate;
-      variants.visible = { ...variants.visible, y: 0 } as Animate;
+        if (hiddenY) {
+            variants.hidden = { ...variants.hidden, y: hiddenY } as Animate;
+            variants.visible = { ...variants.visible, y: 0 } as Animate;
+        }
+        if (hiddenX) {
+            variants.hidden = { ...variants.hidden, x: hiddenX } as Animate;
+            variants.visible = { ...variants.visible, x: 0 } as Animate;
+        }
+        if (scale) {
+            variants.hidden = { ...variants.hidden, scale: scale } as Animate;
+            variants.visible = { ...variants.visible, scale: 1 } as Animate;
+        }
+        return variants;
     }
-    if (hiddenX) {
-      variants.hidden = { ...variants.hidden, x: hiddenX } as Animate;
-      variants.visible = { ...variants.visible, x: 0 } as Animate;
-    }
-    if (scale) {
-      variants.hidden = { ...variants.hidden, scale: scale } as Animate;
-      variants.visible = { ...variants.visible, scale: 1 } as Animate;
-    }
-    return variants;
-  }
-  function getTransition() {
-    let transition = {};
-    if (duration) {
-      transition = { ...transition, duration: duration };
-    }
-    if (delay) {
-      transition = { ...transition, delay: delay };
-    }
+    function getTransition() {
+        let transition = {};
+        if (duration) {
+            transition = { ...transition, duration: duration };
+        }
+        if (delay) {
+            transition = { ...transition, delay: delay };
+        }
 
-    return transition;
-  }
-  return (
-    <>
-      <motion.div
-        variants={getVariant()}
-        initial={initial}
-        animate={mainControls}
-        exit={"hidden"}
-        ref={ref}
-        className={className || "relative w-fit"}
-        transition={getTransition()}
-      >
-        {children}
-      </motion.div>
-      {preanimation && (
-        <motion.div
-          variants={{
-            hidden: { left: 0 },
-            visible: { left: "100%" },
-          }}
-          initial={initial}
-          animate={slideControls}
-          transition={{ duration: 0.5, ease: "easeIn" }}
-          style={{
-            position: "absolute",
-            top: 4,
-            bottom: 4,
-            left: 0,
-            right: 0,
-            background: "#8a2be2",
-            zIndex: 20,
-          }}
-        />
-      )}
-    </>
-  );
+        return transition;
+    }
+    return (
+        <>
+            <motion.div
+                variants={getVariant()}
+                initial={initial}
+                animate={mainControls}
+                exit={'hidden'}
+                ref={ref}
+                className={className || 'relative w-fit'}
+                transition={getTransition()}
+            >
+                {children}
+            </motion.div>
+            {preanimation && (
+                <motion.div
+                    variants={{
+                        hidden: { left: 0 },
+                        visible: { left: '100%' },
+                    }}
+                    initial={initial}
+                    animate={slideControls}
+                    transition={{ duration: 0.5, ease: 'easeIn' }}
+                    style={{
+                        position: 'absolute',
+                        top: 4,
+                        bottom: 4,
+                        left: 0,
+                        right: 0,
+                        background: '#8a2be2',
+                        zIndex: 20,
+                    }}
+                />
+            )}
+        </>
+    );
 }
 
 export default Reveal;
